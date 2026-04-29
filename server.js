06675ec8b4201495dca142179e60ad9f977a9380
karimkadwal-bit <karimkadwal@gmail.com>
2026-04-29 14:39:46 +0430

    update database connection

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 app.use(express.json());
 
+// MongoDB connect
+mongoose.connect("mongodb+srv://karim:ZdgHjYsrs0COMMp7@cluster0.70ffkmb.mongodb.net/")
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log(err));
+
+// Model
+const Product = mongoose.model("Product", {
+  name: String,
+  price: Number
+});
+
+// routes
 app.get("/", (req, res) => {
   res.send("Kadwal Marketplace Server 🚀");
 });
 
-app.get("/products", (req, res) => {
-  res.json([
-    { id: 1, name: "Phone", price: 200 },
-    { id: 2, name: "Laptop", price: 800 }
-  ]);
+app.get("/products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
 });
 
-app.post("/products", (req, res) => {
-  const product = req.body;
-  res.json({
-    message: "Product added successfully ✅",
-    product: product
-  });
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.json({ message: "Saved to DB ✅", product });
 });
 
+// server
 const PORT = process.env.PORT || 4242;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
