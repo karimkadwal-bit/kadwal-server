fef558c9507403bbba5ebe5d5fb2bf4c6515229b
karimkadwal-bit <karimkadwal@gmail.com>
2026-05-16 15:33:21 +0430

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = "YOUR_MONGODB_URI";
+const uri = "PASTE_YOUR_MONGODB_URI_HERE";
 
 const client = new MongoClient(uri);
 
@@ -15,11 +15,19 @@ let db;
 
 async function connectDB() {
 
-  await client.connect();
+  try {
 
-  db = client.db("kadwalDB");
+    await client.connect();
 
-  console.log("MongoDB Connected");
+    db = client.db("kadwalDB");
+
+    console.log("MongoDB Connected");
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
 
 }
 
@@ -27,45 +35,75 @@ connectDB();
 
 app.get('/products', async (req, res) => {
 
-  const products = await db
-    .collection("products")
-    .find({})
-    .toArray();
+  try {
 
-  res.json(products);
+    const products = await db
+      .collection("products")
+      .find({})
+      .toArray();
+
+    res.json(products);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
 
 });
 
 app.post('/products', async (req, res) => {
 
-  const product = req.body;
+  try {
 
-  await db
-    .collection("products")
-    .insertOne(product);
+    const product = req.body;
 
-  res.json({
-    message: "Product added"
-  });
+    const result = await db
+      .collection("products")
+      .insertOne(product);
+
+    res.json(result);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
 
 });
 
 app.delete('/products/:id', async (req, res) => {
 
-  const id = req.params.id;
+  try {
 
-  await db.collection("products").deleteOne({
-    _id: new ObjectId(id)
-  });
+    const id = req.params.id;
 
-  res.json({
-    message: "Product deleted"
-  });
+    const result = await db
+      .collection("products")
+      .deleteOne({
+        _id: new ObjectId(id)
+      });
+
+    res.json(result);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
 
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
 
 });
