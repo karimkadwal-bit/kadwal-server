2e59e888f76be5386609202653e999da71ca54aa
karimkadwal-bit <karimkadwal@gmail.com>
2026-05-19 15:31:54 +0430

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,31 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-
-  res.send("Kadwal Server Running");
-
-});
+let products = [
+  {
+    name: "iPhone",
+    price: 999,
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+  }
+];
 
 app.get('/products', (req, res) => {
 
-  res.json([
-    {
-      name: "iPhone",
-      price: 999
-    },
-    {
-      name: "Car",
-      price: 100000
-    }
-  ]);
+  res.json(products);
+
+});
+
+app.post('/products', (req, res) => {
+
+  const product = req.body;
+
+  products.push(product);
+
+  res.json({
+    message: "Product added"
+  });
 
 });
 
