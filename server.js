2a4e8930c913d50560d6cdf0496d8357d8d926d6
karimkadwal-bit <karimkadwal@gmail.com>
2026-05-20 14:57:42 +0430

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ app.use(cors());
 app.use(express.json());
 
 let products = [
+
   {
     name: "iPhone",
     price: 999,
     image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+  },
+
+  {
+    name: "Car",
+    price: 100000,
+    image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7"
   }
+
 ];
 
 app.get('/products', (req, res) => {
@@ -32,10 +40,28 @@ app.post('/products', (req, res) => {
 
 });
 
+app.delete('/products/:index', (req, res) => {
+
+  const index = req.params.index;
+
+  products.splice(index, 1);
+
+  res.json({
+    message: "Product deleted"
+  });
+
+});
+
+app.get('/', (req, res) => {
+
+  res.send("Kadwal Marketplace Server Running");
+
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 
-  console.log("Server running");
+  console.log("Server running on port " + PORT);
 
 });
