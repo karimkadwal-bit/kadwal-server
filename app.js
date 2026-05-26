19f05a60aef3b5331a3a5ed9583eb33404cec30e
karimkadwal-bit <karimkadwal@gmail.com>
2026-05-26 14:53:46 +0430

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+document
+.getElementById("signupBtn")
+.addEventListener("click", signup);
+
+async function signup() {
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+console.log(email, password);
+
+}
