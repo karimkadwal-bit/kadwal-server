d85f2c89af7d53f1025a83cf761efad15ed11f95
karimkadwal-bit <karimkadwal@gmail.com>
2026-05-26 13:02:09 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
@@ -9,80 +7,125 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const SECRET = "kadwal_secret";
-
-const uri = "mongodb+srv://karim:karim122@cluster0.70ffkmb.mongodb.net/kadwalDB?retryWrites=true&w=majority";
+const uri =
+"mongodb+srv://karim:karim122@cluster0.70ffkmb.mongodb.net/kadwalDB?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
 let db;
 
-async function connectDB(){
-await client.connect();
-db = client.db("kadwalDB");
-console.log("MongoDB Connected");
+async function connectDB() {
+
+  try {
+
+    await client.connect();
+
+    db = client.db("kadwalDB");
+
+    console.log("MongoDB Connected");
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+
 }
+
 connectDB();
 
-app.get("/", (req,res)=>{
-res.send("Kadwal API Running 🚀");
+app.get("/", (req, res) => {
+
+  res.send("Kadwal Marketplace Server Running");
+
 });
 
-// SIGNUP
-app.post("/signup", async (req,res)=>{
-const {username,password} = req.body;
+app.get("/products", async (req, res) => {
 
-const exist = await db.collection("users").findOne({username});
-if(exist){
-return res.json({message:"User exists"});
-}
+  const products = await db
+    .collection("products")
+    .find({})
+    .toArray();
 
-const hash = await bcrypt.hash(password,10);
+  res.json(products);
 
-await db.collection("users").insertOne({
-username,
-password:hash,
-role:"user"
 });
 
-res.json({message:"Signup success"});
+app.post("/products", async (req, res) => {
+
+  const product = req.body;
+
+  await db
+    .collection("products")
+    .insertOne(product);
+
+  res.json({
+    message: "Product Added"
+  });
+
 });
 
-// LOGIN
-app.post("/login", async (req,res)=>{
-const {username,password} = req.body;
+app.delete("/products/:id", async (req, res) => {
 
-const user = await db.collection("users").findOne({username});
+  const id = req.params.id;
 
-if(!user){
-return res.json({message:"User not found"});
-}
+  await db
+    .collection("products")
+    .deleteOne({
+      _id: new ObjectId(id)
+    });
 
-const ok = await bcrypt.compare(password,user.password);
+  res.json({
+    message: "Product Deleted"
+  });
 
-if(!ok){
-return res.json({message:"Wrong password"});
-}
-
-const token = jwt.sign(
-{ id:user._id, role:user.role },
-SECRET,
-{ expiresIn:"7d" }
-);
-
-res.json({
-message:"Login success",
-token,
-role:user.role
-});
 });
 
-// PRODUCTS (READ ONLY SAFE)
-app.get("/products", async (req,res)=>{
-const data = await db.collection("products").find().toArray();
-res.json(data);
+app.post("/signup", async (req, res) => {
+
+  const user = req.body;
+
+  await db
+    .collection("users")
+    .insertOne(user);
+
+  res.json({
+    message: "Signup Successful"
+  });
+
 });
 
-app.listen(3000, ()=>{
-console.log("Server Running");
+app.post("/login", async (req, res) => {
+
+  const { username, password } = req.body;
+
+  const user = await db
+    .collection("users")
+    .findOne({
+      username,
+      password
+    });
+
+  if (user) {
+
+    res.json({
+      message: "Login Successful"
+    });
+
+  } else {
+
+    res.json({
+      message: "Invalid Credentials"
+    });
+
+  }
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log("Server running on port " + PORT);
+
 });
