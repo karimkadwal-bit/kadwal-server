c5ca26914983f61ec811f68c06a9c3e79dd624a1
karimkadwal-bit <karimkadwal@gmail.com>
2026-05-26 15:10:41 +0430

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,33 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Kadwal Marketplace Server Running");
+
+res.send(
+"Kadwal Marketplace Server Running"
+);
+
 });
 
-app.listen(5000, () => {
-  console.log("Server Started on Port 5000");
+app.post("/signup",(req,res)=>{
+
+const {email,password} = req.body;
+
+console.log(email,password);
+
+res.send("Signup Successful");
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+console.log(
+`Server Running On Port ${PORT}`
+);
+
 });
