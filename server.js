4bb362704a2962d144ce8892e46ebdc269f0efea
karimkadwal-bit <karimkadwal@gmail.com>
2026-06-04 15:28:48 +0430

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(
-  "mongodb+srv://karim:kadwal122@cluster0.70ffkmb.mongodb.net/kadwalMarketplace?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://karim:kadwal1220001220@cluster0.70ffkmb.mongodb.net/kadwalMarketplace?retryWrites=true&w=majority&appName=Cluster0"
 )
 .then(() => {
   console.log("MongoDB Connected");
