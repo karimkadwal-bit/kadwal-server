1a65546f64d163f37ba45778c5d1bd753bec77fd
karimkadwal-bit <karimkadwal@gmail.com>
2026-06-28 14:40:58 +0430

    Update server.js

--- server.js
+++ server.js
@@ -858,17 +858,6 @@ app.post(
 
   }
 );
-
-const PORT =
-  process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-
-  console.log(
-    `Server running on port ${PORT}`
-  );
-
-});
 app.put(
   "/increase-quantity/:id",
   async (req, res) => {
