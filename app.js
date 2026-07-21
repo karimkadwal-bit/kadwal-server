16f7041e894f9e5db2e70bbdc6eff3984e535e95
karimkadwal-bit <karimkadwal@gmail.com>
2026-07-21 14:55:53 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1992,7 +1992,11 @@ async function loadSellerMessages(){
 
   document.getElementById("sellerMessages").innerHTML = "";
 
-  chats.forEach(chat=>{
+  const filteredChats = chats.filter(chat =>
+chat.customerName === selectedCustomer
+);
+
+  filteredChats.forEach(chat=>{
 
     document.getElementById("sellerMessages").innerHTML += `
 
