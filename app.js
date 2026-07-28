1c97b99aaf86919236820180070ae4918f6fe645
karimkadwal-bit <karimkadwal@gmail.com>
2026-07-28 16:09:53 +0430

    Update app.js

--- app.js
+++ app.js
@@ -2111,6 +2111,8 @@ async function loadSellerMessages(){
 
 lastChatCount = filteredChats.length;
 
+  document.getElementById("sellerMessages").innerHTML = "";
+
 filteredChats.forEach(chat=>{
 
 document.getElementById("sellerMessages").innerHTML += `
