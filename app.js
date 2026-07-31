7b057c1e0c1bc5c8c077fdfff0b4c936ca2cc5d1
karimkadwal-bit <karimkadwal@gmail.com>
2026-07-31 08:09:21 +0430

    Update app.js

--- app.js
+++ app.js
@@ -2323,3 +2323,12 @@ async function startSellerRecording() {
   }, 10000);
 
 }
+let selectedCustomer = "";
+
+async function openCustomerChat(customer) {
+    selectedCustomer = customer;
+
+    document.getElementById("customerName").innerText = customer;
+
+    loadSellerMessages();
+             }
